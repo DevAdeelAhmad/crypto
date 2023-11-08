a771dfba5d01f3a8faac63c91ca667e150e9d0c5
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-11-09 01:29:16 +0500

    Small Fixes

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -15,10 +15,10 @@ function Navbar() {
         setIsSideBarActive(!isSideBarActive);
     }
     return (
-        <nav className="fixed top-0 left-0">
-            <header className="flex w-screen justify-between items-center z-[100] p-4 border-b-[1px] !h-[10vh] bg-black/90 border-b-[#525252d9]" style={{ backdropFilter: "10px" }}>
+        <nav className="fixed top-0 left-0 z-[100]">
+            <header className="flex w-screen justify-between items-center z-[100] p-4 border-b-[1px] !h-[10vh] bg-black border-b-[#525252d9]">
                 <div className="flex gap-x-2 items-center">
-                    <button className="" onClick={handleSideIconToggle}>
+                    <button onClick={handleSideIconToggle}>
                         {isSideBarActive ? <AiOutlineMenuFold size={25} color="#43ff64d9" /> : <AiOutlineMenu size={25} color="#43ff64d9" />}
                     </button>
                     <Link href='/' className="flex gap-x-2 items-center">
@@ -26,7 +26,7 @@ function Navbar() {
                         <span className="font-semibold text-[#43ff64d9]">CryptoSale</span>
                     </Link>
                 </div>
-                <div className="flex gap-x-2 items-center">
+                <div className="hidden sm:flex gap-x-2 items-center">
                     <Link target="_blank" className="rounded-full px-3 py-2 text-white flex border-2 border-gray-700 gap-x-2 text-sm text-center items-center font-semibold" href='https://www.tradingview.com/'>
                         <GiChart size={25} color="#43ff64d9" />
                         TradingView
@@ -40,7 +40,7 @@ function Navbar() {
                     </Link>
                 </div>
             </header>
-            <aside className={`hidden md:block sticky !top-[80px] left-0 max-h-max !h-[90vh] overflow-y-scroll bg-black/90 ${isSideBarActive ? '!w-[170px]' : '!w-[60px]'}`} style={{ backdropFilter: "10px" }}>
+            <aside className={`sticky !top-[80px] left-0 max-h-max !h-[90vh] z-[100] overflow-y-scroll bg-black ${isSideBarActive ? '!w-[170px]' : 'w-0 md:!w-[60px]'}`}>
                 {isSideBarActive ? <SidebarFull /> : <Sidebar />}
             </aside>
 
--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -6,7 +6,7 @@ function Sidebar() {
     <div className="flex flex-col gap-y-5 py-7 px-2">
       {
         sidebarData.map((data, index) => (
-          <Link className="flex items-center gap-x-2 text-sm" key={index} href={data.href}>
+          <Link className="hidden md:flex items-center gap-x-2 text-sm" key={index} href={data.href}>
             <data.icon size={30} color="#43ff64d9" />
           </Link>
         ))
