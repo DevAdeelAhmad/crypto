533aedd9eb62664d1d48dcc5d72f828d31730461
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-11-09 01:40:47 +0500

    Navbar Links Responsiveness done

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -2,9 +2,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react"
-import { AiOutlineMenuFold, AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineMenuFold, AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 import { GiChart } from 'react-icons/gi'
 import { FaEthereum } from 'react-icons/fa'
+import { BiMenuAltRight } from 'react-icons/bi'
 import Sidebar from "@/components/ui/Sidebar";
 import SidebarFull from "@/components/ui/SidebarFull";
 
@@ -13,6 +14,9 @@ function Navbar() {
     const [isActive, setIsActive] = useState(false);
     function handleSideIconToggle() {
         setIsSideBarActive(!isSideBarActive);
+    }
+    function handleLinkToggle() {
+        setIsActive(!isActive);
     }
     return (
         <nav className="fixed top-0 left-0 z-[100]">
@@ -39,6 +43,28 @@ function Navbar() {
                         Connect
                     </Link>
                 </div>
+
+                <div className="flex sm:hidden items-center justify-center">
+                    <button onClick={handleLinkToggle}>
+                        {isActive ? <AiOutlineClose size={30} color="#43ff64d9" /> : <BiMenuAltRight size={30} color="#43ff64d9" />}
+                    </button>
+                </div>
+
+                {isActive && (
+                    <div className="absolute top-[10vh] right-0 w-screen items-end justify-center flex flex-col gap-y-5 px-5 py-5 bg-black sm:hidden">
+                        <Link target="_blank" className="rounded-full px-3 py-2 text-white flex border-2 border-gray-700 gap-x-2 text-sm text-center items-center font-semibold" href='https://www.tradingview.com/'>
+                            <GiChart size={25} color="#43ff64d9" />
+                            TradingView
+                        </Link>
+                        <Link target="_blank" className="rounded-full px-3 py-2 text-white flex border-2 border-gray-700 gap-x-2 text-sm text-center items-center font-semibold" href=''>
+                            <FaEthereum size={25} color="#43ff64d9" />
+                            Network
+                        </Link>
+                        <Link className="rounded-full px-3 py-2 text-white border-2 bg-[#43ff64d9]/50 border-gray-700 gap-x-2 text-base text-center font-semibold" href='/connect'>
+                            Connect
+                        </Link>
+                    </div>
+                )}
             </header>
             <aside className={`sticky !top-[80px] left-0 max-h-max !h-[90vh] z-[100] overflow-y-scroll bg-black ${isSideBarActive ? '!w-[170px]' : 'w-0 md:!w-[60px]'}`}>
                 {isSideBarActive ? <SidebarFull /> : <Sidebar />}
