caa10cda3d2720eca10c39b0e359107e3f75cbfe
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-11-14 10:54:58 +0500

    banner fixed

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,11 +3,11 @@ const Banner = () => {
     return (
         <div suppressHydrationWarning className="banner">
             <p className="marquee uppercase">
-                <h1 className="text-orange-400 inline-block">GROK + <p className="inline-block text-[#24c077]">172.54%&emsp;</p>GROK + <p className="inline-block text-[#24c077]">172.54%&emsp;</p>GROK + <p className="inline-block text-[#24c077]">172.54%&emsp;</p>GROK + <p className="inline-block text-[#24c077]">172.54%&emsp;</p>GROK + <p className="inline-block text-[#24c077]">172.54%&emsp;</p>GROK + <p className="inline-block text-[#24c077]">172.54%&emsp;</p>GROK + <p className="inline-block text-[#24c077]">172.54%&emsp;</p>GROK + <p className="inline-block text-[#24c077]">172.54%</p>&emsp;&nbsp;</h1>
+                <h1 className="text-orange-400 inline-block">GROK + 172.54%&emsp;GROK + 172.54%&emsp;GROK + 172.54%&emsp;GROK + 172.54%&emsp;GROK + 172.54%&emsp;GROK + 172.54%&emsp;GROK + 172.54%&emsp;GROK + 172.54%&emsp;&nbsp;</h1>
             </p>
             <p className="marquee marquee2 uppercase">
-                <h1 className="text-orange-400 inline-block">GROK + <p className="inline-block text-[#24c077]">172.54%&emsp;</p>GROK + <p className="inline-block text-[#24c077]">172.54%&emsp;</p>GROK + <p className="inline-block text-[#24c077]">172.54%&emsp;</p>GROK + <p className="inline-block text-[#24c077]">172.54%&emsp;</p>GROK + <p className="inline-block text-[#24c077]">172.54%&emsp;</p>GROK + <p className="inline-block text-[#24c077]">172.54%&emsp;</p>GROK + <p className="inline-block text-[#24c077]">172.54%&emsp;</p>GROK + <p className="inline-block text-[#24c077]">172.54%</p>&emsp;&nbsp;</h1>
-            </p >
+                <h1 className="text-orange-400 inline-block">GROK + 172.54%&emsp;GROK + 172.54%&emsp;GROK + 172.54%&emsp;GROK + 172.54%&emsp;GROK + 172.54%&emsp;GROK + 172.54%&emsp;GROK + 172.54%&emsp;GROK + 172.54%&emsp;&nbsp;</h1>
+            </p>
         </div >
     );
 };
